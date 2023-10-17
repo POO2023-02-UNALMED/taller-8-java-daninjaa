def907b5021c6baedb9ff601b8dfa72c6b4a750a
daninjaa <dagarciat@unal.edu.co>
2023-10-16 19:59:35 -0500

    nuevo

--- src/main/java/futbol/Futbolista.java
+++ src/main/java/futbol/Futbolista.java
@@ -18,24 +18,6 @@ public abstract class Futbolista implements Comparable<Futbolista> {
 	public String toString() {
 		return String.format("El futbolista %s tiene %d, y juega de posicion %s", this.nombre, this.edad, this.posicion); 
 	}
-	@Override
-	public abstract int compareTo(Futbolista f) {
-		if (this.nombre != f.nombre) {
-			return 1;
-		}
-		
-		else if (this.edad != f.edad){
-			return 1;
-		}
-		
-		else if(this.posicion != f.posicion){
-			return 1;
-		}
-		else{ 
-			return 0;
-		}
-
-	}
 	
 	public boolean equals(Futbolista f) {
 		if (this.nombre != f.nombre) {
